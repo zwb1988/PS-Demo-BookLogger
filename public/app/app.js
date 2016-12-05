d892b06c9ba6e119945aab400b1d70dab5c54e18
zwb1988 <zwb1988@hotmail.com>
2016-12-05 14:14:01 +0800

    Added new books provider with books service injected

--- public/app/app.js
+++ public/app/app.js
@@ -8,4 +8,32 @@
 (function () {
     var app = angular.module('app', []);
 
+    // Instead of creating provider in the configuration section with injected
+    // $provide, we can use the provider method exposed with the module.
+    // E.g. $provide.provider() => app.provider()
+    app.provider('books', function () {
+        this.$get = function () {
+            var appName = 'Book Logger';
+            var appDesc = 'Track which books you read.';
+
+            var version = '1.0';
+            if (includeVersionInTitle) {
+                appName += ' ' + version;
+            }
+
+            return {
+                appName: appName,
+                appDesc: appDesc
+            };
+        };
+
+        var includeVersionInTitle = false;
+        this.setIncludeVersionInTitle = function (value) {
+            includeVersionInTitle = value;
+        };
+    });
+
+    app.config(function (booksProvider) {
+        booksProvider.setIncludeVersionInTitle(true);
+    });
 }());
--- public/app/books/BooksController.js
+++ public/app/books/BooksController.js
@@ -9,7 +9,9 @@
     angular.module('app').controller('BooksController', BooksController);
 
     function BooksController(books) {
+        var vm = this;
 
+        vm.appName = books.appName;
     }
     ;
 }());
